da89cd9bfef1577f8bb83ce71ea56fb9347ae42f
Dzianis Khatsuk <Dzianis_Khatsuk@epam.com>
2022-10-06 13:42:26 +0300

    feat: add android splash screen

--- android/app/src/main/java/com/animationshowcase/MainActivity.java
+++ android/app/src/main/java/com/animationshowcase/MainActivity.java
@@ -1,6 +1,8 @@
 package com.animationshowcase;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
@@ -13,6 +15,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "AnimationShowcase";
+  }
+
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this, R.style.SplashScreenTheme, true);
+    super.onCreate(savedInstanceState);
   }
 
   /**
